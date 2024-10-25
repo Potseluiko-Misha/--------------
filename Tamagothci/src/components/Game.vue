93677ee553466c5e25aff09e253adad5afed7a16
Misha <vechnuignor@mail.ru>
2024-10-25 17:43:36 +0300

    addEggOnMain

--- Tamagothci/src/components/Game.vue
+++ Tamagothci/src/components/Game.vue
@@ -2,7 +2,9 @@
     <v-container>
         <v-row>
             <v-col>
-                <div>Game</div>
+                <div class="egg">
+
+                </div>
             </v-col>
         </v-row>
     </v-container>
@@ -11,3 +13,13 @@
 <script setup>
 
 </script>
+
+
+<style>
+.egg {
+    background-color: white;
+    height: 100vh;
+    aspect-ratio: 3 / 4;
+    border-radius: 100% / 125% 125% 80% 80%;
+}
+</style>
