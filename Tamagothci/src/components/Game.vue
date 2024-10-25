f62350c501db31362aa90e095411167d7b9de1eb
Misha <vechnuignor@mail.ru>
2024-10-25 17:39:06 +0300

    centerElement

--- Tamagothci/src/components/Game.vue
+++ Tamagothci/src/components/Game.vue
@@ -1,5 +1,11 @@
 <template>
-
+    <v-container>
+        <v-row>
+            <v-col>
+                <div>Game</div>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script setup>
