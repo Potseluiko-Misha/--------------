e34cbb349b2165ba91e85f319854f245bb27d69d
Misha <vechnuignor@mail.ru>
2024-10-27 15:56:24 +0300

    addThreeClasses

--- Tamagothci/src/components/Screen.vue
+++ Tamagothci/src/components/Screen.vue
@@ -1,5 +1,9 @@
 <template>
-
+    <div>
+        <div class="screen_header"></div>
+        <div class="pet"></div>
+        <div class="screen_action"></div>
+    </div>
 </template>
 
 <script setup>
