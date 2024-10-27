835847287febbad1b1de334748a213df2c80ca91
Misha <vechnuignor@mail.ru>
2024-10-27 17:05:58 +0300

    addState

--- Tamagothci/src/store/index.ts
+++ Tamagothci/src/store/index.ts
@@ -1,5 +1,13 @@
 import { reactive } from 'vue'
 
 export const store = reactive({
-    count: 0
+    name: null,
+    state: {
+        happiness: 0,
+        hungry: 0,
+        energy: 0,
+        healhty: 0,
+        hygiene: 0
+    },
+    isDead: false
 })
