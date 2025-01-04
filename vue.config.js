6478629a981b025ad0900e571449e64300c5c8ba
Misha <vechnuignor@mail.ru>
2025-01-04 15:23:38 +0300

    commit

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/<https://github.com/Potseluiko-Misha/-------------->/' : '/'
+    publicPath: process.env.NODE_ENV === 'production' ? '/https://github.com/Potseluiko-Misha/--------------/' : '/'
 }
